fe2978d3293bc0ad331bb0d34777db2bbeaff170
Premkumar-hulikoppe <premuhulikoppe@gmail.com>
2022-11-20 16:13:33 +0530

    inititated react-app

---
+++ posts-management/src/firebase/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAsfaQOigYNJ98ovRnBgx2hr_dnLh0GCjU",
+  authDomain: "xhipment-95213.firebaseapp.com",
+  projectId: "xhipment-95213",
+  storageBucket: "xhipment-95213.appspot.com",
+  messagingSenderId: "1023755536957",
+  appId: "1:1023755536957:web:6eafa2b2d74a8289c97854",
+  measurementId: "G-Y5WPD339JR",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
